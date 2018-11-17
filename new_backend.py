d56a8b3b1564f6b222be3034efa033b7433141da
Josef Klafka <jlklafka@gmail.com>
2018-11-16 18:40:56 -0600

    data_box implementation drafted

--- new_backend.py
+++ new_backend.py
@@ -7,38 +7,50 @@ class data_box(object):
 	'''
 	Abstract representation of each box. The boxes represent any 
 	organizing unit within the final output of the program, from 
-	the largest box containing everything else	to the boxes containing 
+	the largest box containing everything else to the boxes containing 
 	groups of stems or suffixes. 
 	'''
-	def __init__(self, name, my_type, text):
-		self.name = name
+	def __init__(self, my_type, text):
 		self.my_type = my_type # "row" or "stack" 
-		self._inner_boxes = [] # list of data_box objects
+		self.inner_boxes = [] # list of data_box objects
 		self._num_inner = 0
-		self.text = text # can be empty, or stems or suffixes
+		self.text = text # can be empty, or list of stems/suffixes
+
 
 	def include_box(self, inner_box):
 		self._num_inner += 1
-		self._inner_boxes.append([inner_box, self._num_inner])
+		self.inner_boxes.append([inner_box, self._num_inner])
 		return 
 
-	def sort(self, criterion, type):
-		## sort by number of suffixes
-		## sort by number of stems
-		## sort by robustness
+
+	def sort(self, crit, reverse_tf = False):
+		if self.my_type == "stack" and self.text is None: #is outer_box
+			if crit == "suffixes":
+				self.inner_boxes.sort(lambda x: x[1], reverse = reverse_tf)
+			elif crit == "stems":
+				self.inner_boxes.sort(lambda x: x[0], reverse = reverse_tf)
+			elif crit == "robustness":
+				self.inner_boxes.sort(lambda x: x[2], reverse = reverse_tf)
 
 
-	def filter(self, criterion, type):
-		## filter by number of suffixes
+	def filter(self, crit, level):
+		if self.my_type == "stack" and self.text is None: #is outer_box
+			if crit == "suffixes":
+				self.inner_boxes = [box for box in self.inner_boxes \
+					if box[1][1] >= level]
+			elif crit == "stems":
+				self.inner_boxes = [box for box in self.inner_boxes \
+					if box[1][0] >= level]
+			elif crit == "robustness":
+				self.inner_boxes = [box for box in self.inner_boxes \
+					if box[1][2] >= level]
+			elif crit == "certain suffixes":
+				self.inner_boxes = [box for box in self.inner_boxes \
+					if suffix in box[0].inner_boxes[1][0] for suffix in level]
+
 		## filter by whether the signature contains certain suffixes
 
-
-	def get_inner_boxes(self):
-		'''
-		Returns a list of the names of the boxes in self._inner_boxes
-		'''
-		return [item[0] for item in self._inner_boxes]
-
+"""
 	def delete_inner_box(self, box_name):
 		'''
 		Given the name of an inner box (a box that lives inside the data_box, 
@@ -47,33 +59,66 @@ class data_box(object):
 		self._inner_boxes = [item for item in self._inner_boxes \
 			if item[0].name != box_name]
 		return
-
+"""
 
 class graphics_box(object):
 	"""
 	The actual graphical representation of a group of suffixes, a group of
 	stems or a group of smaller boxes. 
 	"""
-	def __init__(self, data_box, svg, drawn):
+	def __init__(self, data_box, svg):
 		self._inner_boxes = []
-		self._draw_box(data_box.text, upper_left, box_size, svg)
+		self._svg
+		self.data_box = data_box
+		#self._draw_box(data_box.text, upper_left, box_size, svg)
+		#for inner_box in data_box.inner_boxes:
+		#	self._inner_boxes.append(graphics_box(inner_box[0], svg))
 
-		for inner_box in data_box.get_inner_boxes():
-			self._inner_boxes.append(graphics_box(inner_box[0], svg, drawn))
-
-	def _draw_box(self, text, upper_left, box_size, svg):
-		svg.add(svg_document.rect(insert = upper_left,
+	def draw_box(self, upper_left, box_size):
+		self._svg.add(self._svg.rect(insert = upper_left,
                                    size = box_size,
                                    stroke_width = "5",
                                    stroke = "black",
                                    fill = "rgb(128,0,0)"))
+	def add_text(self, text, location):
+		self._svg.add(self._svg.text(text, \
+	                                   insert = location))
 
 	#def zoom(self, percentage):
 	## still have to implement
 
+def get_robustness(stem_box, suffix_box, num_stems, num_suffixes):
+	n0 = 0
+	n1 = 0
+	num_letters = 0
+	for item in stem_box.text:
+		n0 += len(item)
+	num_letters += n0
+	n0 *= num_suffixes
+	for item in suffix_box.text:
+		n1 += len(item)
+	num_letters += n1
+	n1 *= num_stems
+	n2 = n1 + n0
+	return n2 - num_letters
+
+
+def make_sig_box(signature):
+	sig_box = data_box("row", None)
+	stem_box = data_box("stack", list(signatures.values()))
+	suffix_box = data_box("stack", list(signatures.keys()))
+	num_stems = len(stem_box.text)
+	num_suffixes = len(suffix_box.text)
+	sig_box.include_box(stem_box)
+	sig_box.include_box(suffix_box)
+	sig_box.text = [num_stems, num_suffixes, get_robustness(stem_box, \
+		suffix_box, num_stems, num_suffixes)]
+	return sig_box
+
 
 def main(signatures_filename, font_size):
 	signatures = crab_nebula_klafka.main(signatures_filename) # dict maps suffixes to stems
+	'''
 	suff_dict = {}
 	stem_dict = {}
 	stem_str = ""
@@ -90,10 +135,17 @@ def main(signatures_filename, font_size):
 	file.close()
 	stem_sigs = crab_nebula_klafka.main("stems.txt")
 	#subprocess.call(["rm", "stems.txt"])
-
-
-	svg_document = svgwrite.Drawing(filename = "test-svgwrite.svg",
+	'''
+	outer_box = data_box("stack", None)
+	for sig in signatures:
+		outer_box.include_box(make_sig_box(sig))
+	for box_pair in outer_box.inner_boxes:
+		box_pair[1] = box_pair[0].text
+	svg_document = svgwrite.Drawing(filename = "crab_nebula.svg",
                             size = ("1000px", "1000px"))
+	for box in outer_box.inner_boxes:
+
+		if box[0].inner_boxes != []:
 
 if __name__ = "__main__":
 	main(sys.argv[1], sys.argv[2])
