303466ed9b11456098c52e55b21dd3a385b41b4b
jklafka <33164351+jklafka@users.noreply.github.com>
2019-01-17 20:08:44 -0600

    added filter

--- new_backend.py
+++ new_backend.py
@@ -10,41 +10,44 @@ BASE_SIZE = 12
 
 class data_box(object):
 	'''
-	Abstract representation of each box. The boxes represent any 
-	organizing unit within the final output of the program, from 
-	the largest box containing everything else to the boxes containing 
-	groups of stems or suffixes. 
+	Abstract representation of each box. The boxes represent any
+	organizing unit within the final output of the program, from
+	the largest box containing everything else to the boxes containing
+	groups of stems or suffixes.
 	'''
 	def __init__(self, my_type, text):
-		self.my_type = my_type # "row" or "stack" 
+		self.my_type = my_type # "row" or "stack"
 		self.inner_boxes = [] # list of data_box objects
 		self._num_inner = 0
-		self.text = text # can be empty, or list of stems/suffixes
+		self.text = text # used for stems/suffixes at lowest level
 
 
 	def include_box(self, inner_box):
 		'''
 		Adds an additional inner_box under data_box paired with how
-		many inner boxes were added previously. 
+		many inner boxes were added previously.
 		'''
 		self._num_inner += 1
 		self.inner_boxes.append([inner_box, self._num_inner])
-		return 
+		return
 
 
 	def sort(self, crit, reverse_tf = False):
 		'''
-		[Currently not in use] Sort the inner_boxes according to criterion 
-		crit, in reverse if reverse_tf is True. 
+		[Currently not in use] Sort the inner_boxes according to criterion
+		crit, in reverse if reverse_tf is True.
 		'''
 		if self.my_type == "stack" and self.text is None: #is outer_box
 			if crit == "suffixes":
-				self.inner_boxes.sort(lambda x: x[0].text[1], reverse = reverse_tf)
+				self.inner_boxes.sort(lambda x: x[0].text[1], \
+				reverse = reverse_tf)
 			elif crit == "stems":
-				self.inner_boxes.sort(lambda x: x[0].text[0], reverse = reverse_tf)
+				self.inner_boxes.sort(lambda x: x[0].text[0], \
+				reverse = reverse_tf)
 			elif crit == "robustness":
-				self.inner_boxes.sort(lambda x: x[0].text[2], reverse = reverse_tf)
-		return 
+				self.inner_boxes.sort(lambda x: x[0].text[2], \
+				reverse = reverse_tf)
+		return
 
 
 	def filter(self, crit, level):
@@ -70,7 +73,7 @@ class data_box(object):
 class graphics_box(object):
 	"""
 	The actual graphical representation of a group of suffixes, a group of
-	stems or a group of smaller boxes. 
+	stems or a group of smaller boxes.
 	"""
 	def __init__(self, data_box, svg, upper_left, box_size):
 		#graphics_box.LAYER_NUM += 1
@@ -86,7 +89,8 @@ class graphics_box(object):
 				self._inner_boxes.append(graphics_box(inner_box[0], svg, \
 					((upper_left[0] + BASE_SIZE) + (box_size[0] /  num_inner) * i, \
 						upper_left[1] + BASE_SIZE), \
-					((box_size[0] / num_inner) -  2 * BASE_SIZE, box_size[1] -  2 * BASE_SIZE)))
+					((box_size[0] / num_inner) -  2 * BASE_SIZE, \
+						box_size[1] -  2 * BASE_SIZE)))
 				i += 1
 		else:
 			words_list = []
@@ -94,13 +98,14 @@ class graphics_box(object):
 					min(5, len(self.data_box.text) - 1)):
 				words_list.append(self.data_box.text[num])
 			self.add_text(self._svg, words_list, \
-				(upper_left[0] + 2 * BASE_SIZE, upper_left[1] + box_size[1] / 2)) 
+				(upper_left[0] + 2 * BASE_SIZE, \
+				upper_left[1] + box_size[1] / 2))
 
 
-	def draw_box(self, svg, upper_left, box_size): 
+	def draw_box(self, svg, upper_left, box_size):
 		'''
 		Draw box of size box_size in pixels with upper left corner located
-		at coordinates upper_left in pixels on canvas svg. 
+		at coordinates upper_left in pixels on canvas svg.
 		'''
 		svg.add(svg.rect(insert = upper_left,
                                    size = box_size,
@@ -111,16 +116,16 @@ class graphics_box(object):
 
 	def add_text(self, svg, text, location):
 		'''
-		Draw text at location in pixels on canvas svg. 
+		Draw text at location in pixels on canvas svg.
 		'''
 		svg.add(svg.text(text, insert = location))
 
 
 def get_robustness(stem_box, suffix_box, num_stems, num_suffixes):
 	'''
-	Compute the robustness of a signature data_box with stems in stem_box, 
+	Compute the robustness of a signature data_box with stems in stem_box,
 	suffixes in suffix_box, number of stems num_stems and number of suffixes
-	num_suffixes. 
+	num_suffixes.
 	'''
 	n0 = 0
 	n1 = 0
@@ -139,9 +144,9 @@ def get_robustness(stem_box, suffix_box, num_stems, num_suffixes):
 
 def make_sig_box(suffixes, stems):
 	'''
-	Given a tuple of stems and a tuple of suffixes, 
+	Given a tuple of stems and a tuple of suffixes,
 	create a data_box with a data_box containing the stems
-	and a data_box containing the suffixes as inner_boxes. 
+	and a data_box containing the suffixes as inner_boxes.
 	'''
 	sig_box = data_box("row", None)
 	stem_box = data_box("stack", list(stems))
@@ -157,27 +162,37 @@ def make_sig_box(suffixes, stems):
 
 def main(signatures_filename, sort_crit=None, filter_crit=None, zoom=100):
 	'''
-	Construct the data_boxes and their graphical graphics_box representations. 
+	Construct the data_boxes and their graphical graphics_box representations.
 	Then draw the graphics_boxes on an SVG image.
 	'''
-	zoom = float(zoom/100)
-	signatures = gui_crab_nebula.main(signatures_filename) # dict maps suffixes to stems
+	zoom = float(zoom)
+	signatures = gui_crab_nebula.main(signatures_filename)
 	outer_box = data_box("stack", None)
 	outer_box.text = 0
 	for key, val in signatures.items():
 		outer_box.include_box(make_sig_box(key, val))
 		outer_box.text += 1
 
-	if filter_crit is not "None":
+	if filter_crit is not None:
 		filter_list = filter_crit.split('=')
+		if filter_list[0] == "stem":
+			outer_box.inner_boxes = [box for box in outer_box.inner_boxes \
+				if filter_list[1] in box[0].inner_boxes[0][0].text]
+		if filter_list[0] == "suffix":
+			outer_box.inner_boxes = [box for box in outer_box.inner_boxes \
+				if filter_list[1] in box[0].inner_boxes[1][0].text]
+		j = 0
+		for box_pair in outer_box.inner_boxes:
+			box_pair[1] = j
+			j += 1
 
-	if sort_crit is not None: 
+	if sort_crit is not None:
 		if sort_crit == "robustness":
-			outer_box.inner_boxes.sort(lambda x : x[0].text[2])
+			outer_box.inner_boxes.sort(key=lambda x : x[0].text[2])
 		elif sort_crit == "stems":
-			outer_box.inner_boxes.sort(lambda x : x[0].text[0])
+			outer_box.inner_boxes.sort(key=lambda x : x[0].text[0])
 		elif sort_crit == "suffixes":
-			outer_box.inner_boxes.sort(lambda x : x[0].text[1])		
+			outer_box.inner_boxes.sort(key=lambda x : x[0].text[1])
 
 	svg_document = svgwrite.Drawing(filename = "crab_nebula.svg",
                             size = (1000 * zoom, \
